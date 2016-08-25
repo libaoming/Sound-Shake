1a1d3dcb192028e5bf534e74783a3dd8c6bf172e
master <libaoming2@gmail.com>
2016-08-25 17:10:39 +0800

    complete sound shaker

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let soundArray = ["bell1","bell2","bell3","bell4","bell5","bell6","bell7"]
+            
+            let rand = Int(arc4random_uniform(UInt32(soundArray.count)))
+            
+            let fileName = soundArray[rand]
+            
+            let filePath = Bundle.main.path(forResource: fileName, ofType: "mp3")
+            
+            do {
+                
+               try  player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: filePath!))
+                
+                player.play()
+                
+                
+            } catch {
+                
+            }
+            
+        }
+    }
 
 }
 
